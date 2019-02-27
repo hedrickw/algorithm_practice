8a6e9e01403e9a98f5c50f431ff342ed2b9d26b3
RallyCapCoder <rallycapcoding@gmail.com>
2019-02-27 08:56:32 -0500

    basic bfs implementation

---
+++ BFS/bfs.py
@@ -0,0 +1,27 @@
+from collections import deque
+graph = {}
+graph['you'] = ["alice","bob","claire"]
+graph['bob'] = ['anuj', 'peggy']
+graph['alice'] = ['peggy']
+graph['claire'] = ['thom','jonny']
+graph['anuj'] = []
+graph['peggy'] = []
+graph['thom'] = []
+graph['jonny'] = []
+
+search_queue = deque()
+search_queue += graph["you"]
+searched = []
+
+def person_is_seller(person):
+    return person[-1] == 'm'
+
+while search_queue:
+    person = search_queue.popleft()
+    if person_is_seller(person):
+        print(f"{person} is a mango seller")
+        exit
+    else:
+        search_queue += graph[person]
+        searched.append(person)
+
